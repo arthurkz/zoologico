16e66a41e70dd032c998042cc00d8bd4b7401290
arthurkz <arthurribeirokz@gmail.com>
2021-09-16 15:10:59 -0300

    implements a new template

--- src/Controller/MamiferoController.php
+++ src/Controller/MamiferoController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MamiferoController{
+class MamiferoController extends AbstractController
+{
 
     /**
      * @Route("/")
@@ -19,12 +21,20 @@ class MamiferoController{
     /**
      * @Route("/mamifero/{slug}")
      */
-    public function show($slug){
-        return new Response(
-            sprintf(
-                'Esta é a página de detalhe do Mamífero "%s"',
-                ucwords(str_replace('-', ' ', $slug))
-            ));
+    public function show($slug)
+    {
+        $respostas = [
+          'Esta é a primeira resposta',
+          'Esta é a segunda resposta',
+          'Esta é a terceira resposta',
+          'Esta é a quarta resposta',
+          'Esta é a quinta resposta'
+        ];
+
+        return $this->render('mamiferos/show.html.twig', [
+            'animal' => ucwords(str_replace('-', ' ', $slug)),
+            'respostas' => $respostas
+        ]);
     }
 
 }
